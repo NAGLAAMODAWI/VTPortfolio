a0330305eb4ccebaebb029274c6b1ce2957008d0
NAGLAAMODAWI <NAGLAAMODAWI>
2021-11-28 21:50:13 -0800

    creat my portfolio project

---
+++ js/main.js
@@ -0,0 +1,106 @@
+$(function(){
+    'use strict';
+    
+//Start Copy Whriter 
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 5) || 500;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
+//Start Window 
+$(window).scroll(function(){
+    //Scroall To Top Function
+    var scrolToTop =  $('.sroal');
+    if($(window).scrollTop() >= 800){
+        scrolToTop.fadeIn(500);
+    }
+    else{
+        scrolToTop.fadeOut(500);
+    }
+       //ServicesData Appear
+    if($(window).scrollTop() >= 1000){
+        $('.Services .ServicesData').slideDown(2000);
+    };
+});//End Window
+
+//Gallary Code
+// Gallery carousel (uses the Owl Carousel library)
+$(".gallery-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    center:true,
+    responsive: { 0: { items: 1 }, 768: { items: 3 }, 992: { items: 4 }, 1200: {items: 5}
+    }
+  });
+
+  // Buy tickets select the ticket type on click
+  $('#buy-ticket-modal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget);
+    var ticketType = button.data('ticket-type');
+    var modal = $(this);
+    modal.find('#ticket-type').val(ticketType);
+  })
+});
+
+ //Load Screen Code
+$(window).load(function(){
+    $("body").css("overflow","auto");
+$(".loading-overlay .spinner").fadeOut(1000,function(){
+$(this).parent().fadeOut(1000,function(){
+    $(this).remove();
+})
+});
+});
